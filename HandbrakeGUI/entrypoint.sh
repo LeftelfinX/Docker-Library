cd9607c09bd03c9d9fb643d92ab734b4564aac97
leftelfinx <leftelfinx@proton.me>
2025-07-27 12:32:40 +0530

    patch

--- HandbrakeGUI/entrypoint.sh
+++ HandbrakeGUI/entrypoint.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-# Create runtime dirs
+# Fix permission for dconf
+sudo mkdir -p /run/user/1000
+sudo chown -R appuser:appuser /run/user/1000
+
+# Create Xpra runtime dirs
 mkdir -p /run/user/1000/xpra
 mkdir -p ~/.xpra
 
-# Fix XDG runtime
 export XDG_RUNTIME_DIR=/run/user/1000
 
-# Start Xpra with HTML5 WebSocket server
+# Start dbus
+eval $(dbus-launch --sh-syntax)
+
+# Start HandBrake GUI in Xpra
 xpra start :0 \
   --bind-tcp=0.0.0.0:10000 \
   --html=on \
